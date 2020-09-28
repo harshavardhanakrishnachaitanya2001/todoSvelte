4654cc582af11d7e2a16e371104da6b088db9926
Harshavardhan <187y1a0575@gmail.com>
2020-09-28 22:26:41 +0530

    Must add a button to every task

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	let thing='';
+	let listOfThings=[];
+	function handleChange(e){
+		e.preventDefault();
+		thing=e.target.value;
+	}
+	function handleClick(e){
+		e.preventDefault();
+		listOfThings.push(thing);
+		listOfThings=listOfThings;
+		thing='';
+	}
+	
+</script>
+<div>
+	<h1>Todo application</h1>
+	<h2>Made by: G. Harshavardhan</h2>
+	<b>task:</b> <input type='text' placeholder='task name' on:change={handleChange} value={thing} required/>
+	<button on:click={handleClick}>Add task</button>
+	<p class='displayListOfThings'>{listOfThings}</p>
+</div>
+
+<style>
+	div {
+		text-align: center;
+	}
+	button{
+		cursor:pointer
+	}
+	input{
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 4em;
+		font-weight: bolder;
+	}
+</style>
